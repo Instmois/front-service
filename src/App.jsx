cfdbfc4eff784e16f3e297a8d297183af4dea5ae
krllvv <krllv@pm.me>
2024-10-21 17:20:56 +0300

    feat: init project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Register from "./Register.jsx";
+import Login from "./Login.jsx";
+
+function App() {
+
+  return (
+    <>
+      <h1>Починим всё</h1>
+        <main>
+            <Register>
+
+            </Register>
+            <Login>
+
+            </Login>
+        </main>
+    </>
+  )
+}
+
+export default App
